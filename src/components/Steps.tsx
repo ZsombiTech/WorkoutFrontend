449a9a74e3abad7c1ac8a615fd2f11b1c6567af3
ZsombiTech <horvathzsombor2005@gmail.com>
2022-04-14 04:03:06 +0200

    e

--- src/components/Steps.tsx
+++ src/components/Steps.tsx
@@ -1,29 +1,14 @@
 import * as React from "react";
+import "../styles/costum.css";
 
 export default function Steps() {
   return (
     <>
       <div>
         <div className="profilebox">
-          <div className="profileflexbox">
-            <div className="profilepicture">
-              <img
-                src="https://images.pexels.com/photos/1704488/pexels-photo-1704488.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-                alt="Profile Picture"
-                className="profilepictureimg"
-              />
-            </div>
-            <div className="profiletext">
-              <h1 className="profiletitle">Username</h1>
-              <h3 className="profiledescripton">Desc</h3>
-              <div className="flexbuttons">
-                <input
-                  type="submit"
-                  className="profilesubmitbutton"
-                  value="Profile"
-                />
-                <div className="profilesubmitbutton2">{`Followers ${2}`}</div>
-              </div>
+          <div className="profileflexbox4">
+            <div className="profiletext2">
+              <h1 className="profiletitle">Steps</h1>
             </div>
           </div>
           <hr />
