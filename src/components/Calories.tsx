cda244b894cebc623d9e1df55730eecab13db4b7
ZsombiTech <horvathzsombor2005@gmail.com>
2022-04-15 04:50:58 +0200

    foods

--- src/components/Calories.tsx
+++ src/components/Calories.tsx
@@ -72,7 +72,41 @@ export default function Calories() {
             <p className="profileshowtasks">Hamburger</p>
             <p className="profileshowtasks">270kcl</p>
           </div>
+          <div className="flexbuttons2">
+            <p className="profileshowtasks">Tuesday</p>
+            <p className="profileshowtasks">Pizza</p>
+            <p className="profileshowtasks">150kcl</p>
+          </div>
+          <div className="flexbuttons2">
+            <p className="profileshowtasks">Tuesday</p>
+            <p className="profileshowtasks">Pizza</p>
+            <p className="profileshowtasks">150kcl</p>
+          </div>
         </div>
+        <div className="profilebox5">
+          <div className="profileflexbox4">
+            <div className="profiletext2">
+              <h1 className="profiletitle">Add meal</h1>
+              <br />
+            </div>
+          </div>
+          <hr />
+          <div className="flexbuttons5">
+            <input
+              type="text"
+              placeholder="Food name"
+              className="stepinput foodinput"
+            />
+            <input
+              type="text"
+              placeholder="Calories"
+              className="stepinput foodinput"
+            />
+            <button className="stepbutton">Add</button>
+          </div>
+          <br />
+        </div>
+        <br />
       </div>
     </>
   );
