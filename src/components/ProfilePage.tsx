abf655223d361ab204963baa43221602b34488cb
Zsombor Horvath <horvathzsombor2005@gmail.com>
2022-05-07 21:32:32 -0700

    work

--- src/components/ProfilePage.tsx
+++ src/components/ProfilePage.tsx
@@ -1,8 +1,17 @@
-import * as React from "react";
+import react, { useState, useEffect } from "react";
 import Avatar from "@mui/material/Avatar";
 import Stack from "@mui/material/Stack";
 
 export default function ProfilePage() {
+  const [username, setUsername] = useState<string>("");
+
+  useEffect(() => {
+    const usernamee = localStorage.getItem("displayName");
+    if (usernamee) {
+      setUsername(usernamee);
+    }
+  }, []);
+
   function stringToColor(string: string) {
     let hash = 0;
     let i;
@@ -48,50 +57,38 @@ export default function ProfilePage() {
         </section>
         <section className="relative py-16 bg-gray-300">
           <div className="container mx-auto px-4">
-            <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-xl rounded-lg -mt-64 justify-center">
+            <div className="relative flex flex-col min-w-0 break-words bg-coollightdark w-full mb-6 shadow-xl rounded-lg -mt-64 justify-center">
               <div className="px-6 justify-center">
                 <Stack
                   direction="row"
                   spacing={2}
                   className="text-center justify-center mt-10"
                 >
-                  <Avatar {...stringAvatar("Zsombitech")} />
+                  <Avatar {...stringAvatar(username)} />
                 </Stack>
 
                 <div className="text-center mt-5">
-                  <h3 className="text-4xl font-semibold leading-normal mb-2 text-gray-800 mb-2">
-                    Jenna Stones
+                  <h3 className="text-4xl font-semibold leading-normal mb-2 text-white mb-2">
+                    {username}
                   </h3>
-                  <div className="text-sm leading-normal mt-0 mb-2 text-gray-500 font-bold uppercase">
-                    <i className="fas fa-map-marker-alt mr-2 text-lg text-gray-500"></i>{" "}
-                    Los Angeles, California
-                  </div>
-                  <div className="mb-2 text-gray-700 mt-10">
-                    <i className="fas fa-briefcase mr-2 text-lg text-gray-500"></i>
-                    Solution Manager - Creative Tim Officer
-                  </div>
-                  <div className="mb-2 text-gray-700">
-                    <i className="fas fa-university mr-2 text-lg text-gray-500"></i>
-                    University of Computer Science
+
+                  <div className="text-sm leading-normal mt-0 mb-2 text-white font-bold uppercase ">
+                    <i className="fas fa-map-marker-alt mr-2 text-lg text-white"></i>
+                    Location
+                    <button className="ml-3 bg-coolpurple">
+                      Change location
+                    </button>
                   </div>
                 </div>
-                <div className="mt-10 py-10 border-t border-gray-300 text-center">
+                <div className="mt-10 py-10 border-t border-white text-center">
                   <div className="flex flex-wrap justify-center">
                     <div className="w-full lg:w-9/12 px-4">
-                      <p className="mb-4 text-lg leading-relaxed text-gray-800">
-                        An artist of considerable range, Jenna the name taken by
-                        Melbourne-raised, Brooklyn-based Nick Murphy writes,
-                        performs and records all of his own music, giving it a
-                        warm, intimate feel with a solid groove structure. An
-                        artist of considerable range.
+                      <p className="mb-4 text-lg leading-relaxed text-white">
+                        Long
                       </p>
-                      <a
-                        href="#pablo"
-                        className="font-normal text-pink-500"
-                        onClick={(e) => e.preventDefault()}
-                      >
-                        Show more
-                      </a>
+                      <button className="bg-coolpurple">
+                        Change Description
+                      </button>
                     </div>
                   </div>
                 </div>
